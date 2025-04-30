2a62fa76100496729344a6a81cc24ac15ab8c746
pepe_icn <antonio.garcia.lattice@gmail.com>
2025-04-30 12:16:28 -0600

    main: changed to newunit

--- src/main.f90
+++ src/main.f90
@@ -9,23 +9,27 @@ program U1_2d
   use create_files
   implicit none
 
-  integer :: i_b
+  integer :: i_b, outfile
   character(:), allocatable :: datafile
-
+  real(dp) :: r
+  
   call read_input
   call set_memory(u,L,beta,beta_i,beta_f,n_beta,plq_action,n_measurements)
   print*, beta
   !print*,sqrt(2/beta)
   !call cold_start(u)
+  call random_number(r)
+  !call sleep(floor(r*10+1))
   call create_measurements_file(L,datafile)
   call hot_start(u,L)
-  open( unit = 10, file = datafile, status = 'unknown')
+  open( newunit = outfile, file = datafile, status = 'old')
   do i_b = 1, n_beta
      call initialization(u,plq_action,beta(i_b),N_thermalization,N_measurements, N_skip)
      call max_jackknife_error_2(plq_action,avr_action,err_action,bins)
      print*,beta(i_b), avr_action, err_action
-     write(10,*) beta(i_b), avr_action, err_action
-     flush(10)
+     write(outfile,*) beta(i_b), avr_action, err_action
+     flush(outfile)
   end do
+  close(outfile)
 
 end program U1_2d
